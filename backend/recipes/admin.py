854e4fcfabe8e55ef6a0cb302e77024271072982
Светлана Ременюк <lan2828@yandex.ru>
2022-12-16 18:42:53 +0300

    rt

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -41,6 +41,18 @@ class RecipeAdmin(admin.ModelAdmin):
     def is_favorited(self, obj):
         return Favorite.objects.filter(recipe=obj).count()
 
+    @staticmethod
+    def amount_favorites(obj):
+        return obj.favorites.count()
+
+    @staticmethod
+    def amount_tags(obj):
+        return "\n".join([i[0] for i in obj.tags.values_list('name')])
+
+    @staticmethod
+    def amount_ingredients(obj):
+        return "\n".join([i[0] for i in obj.ingredients.values_list('name')])
+
 
 @admin.register(Favorite)
 class FavoriteAdmin(admin.ModelAdmin):
